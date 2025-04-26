339d0d6bbf2dd4b1c7d4bf3a1bbf0d46a2242c60
mikuichikamikasa <kalelimas24@gmail.com>
2025-04-26 17:35:35 -0400

    r

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,7 +15,7 @@ const App = () => (
       <LanguageProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/clarity-booking-site">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
